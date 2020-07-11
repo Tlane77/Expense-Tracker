b63e6c3b6c321ee097fbd05c77bd3647b2d24dc8
Tasha Lane <designtmg77@gmail.com>
2020-07-10 21:59:23 -0500

    transactionList

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Header } from './components/Header';
 import { Balance } from './components/Balance';
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
 import './App.css';
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <Header />
       <div className="container">
         <Balance />
+        <IncomeExpenses />
+        <TransactionList />
 
         </div>
     </div>
---
+++ src/components/TransactionList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const TransactionList = () => {
+    return (
+        <>
+            <h3>History</h3>
+            <ul id="list" class="list">
+                {/* Later this will be replaced with transaction component */}
+         <li class="minus">
+          Cash <span>-$400</span><button class="delete-btn">x</button>
+        </li> -->
+      </ul>
+
+        </>
+    )
+}
