b7add9263c49926f1d141f5af728246da7b02e14
Tasha Lane <designtmg77@gmail.com>
2020-07-10 23:14:19 -0500

    globalComponents

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { IncomeExpenses } from "./components/IncomeExpenses";
 import { TransactionList } from "./components/TransactionList";
+import { AddTransaction } from "./components/AddTransaction";
+import { GlobalProvider } from "./components/GlobalProvider";
+
 import './App.css';
+
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
         <IncomeExpenses />
         <TransactionList />
+        <AddTransaction />
 
         </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,6 +1,9 @@
-import React from 'react'
-
+import React,{ useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState';
 export const TransactionList = () => {
+  const context = useContext(GlobalContext);
+
+  console.log(context);
     return (
         <>
             <h3>History</h3>
